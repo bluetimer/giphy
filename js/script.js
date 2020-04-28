9341cc32e8b779dc31f756ed0642cbd4233c58f5
Issac Teoh <teohissac@gmail.com>
2020-04-28 21:50:49 +0000

    add code to giphy api

--- js/script.js
+++ js/script.js
@@ -4,7 +4,21 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+    var choice = $("#search-term").val();
+    var request_url = "https://api.giphy.com/v1/gifs/search?q=" +
+     choice + 
+   "&rating=pg&api_key=dc6zaTOxFJmzC";
+
+  fetch(request_url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+    debugger;
+      var pic_url = data.data[0].images.preview_gif.url;
+
+       $('#results').append('<img src=' + pic_url + '>');
+    })
   
   
 });
